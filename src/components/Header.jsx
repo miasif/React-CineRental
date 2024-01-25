ea9e78a6cffcbc00b21b85baff2d1700fa50847a
miasif <minhazul.islam.asif@gmail.com>
2024-01-26 02:44:37 +0600

    dark mode

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,15 @@
 import React, { useContext, useState } from "react";
 import Moon from "../assets/icons/moon.svg";
+import Sun from "../assets/icons/sun.svg";
 import Logo from "../assets/logo.svg";
 import Ring from "../assets/ring.svg";
 import ShoppingCart from "../assets/shopping-cart.svg";
-import { MovieContext } from "../context";
+import { MovieContext, ThemeContext } from "../context";
 import CartDetails from "./CartDetails";
 
 export default function Header() {
   const [showCart, setShowCart] = useState(false);
-
+  const { darkMode, setDarkMode } = useContext(ThemeContext);
   const { cartData } = useContext(MovieContext);
 
   function handleCartShow() {
@@ -36,8 +37,14 @@ export default function Header() {
             <a
               className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
               href="#"
+              onClick={() => setDarkMode((darkMode) => !darkMode)}
             >
-              <img src={Moon} width="24" height="24" alt="Moon" />
+              <img
+                src={darkMode ? Sun : Moon}
+                width="24"
+                height="24"
+                alt="Moon"
+              />
             </a>
           </li>
           <li>
